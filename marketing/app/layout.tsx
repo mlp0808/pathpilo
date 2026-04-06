0e8fdfbb9c8acd416214ef99c48143364074bad5
Mikkel Pallesen <mikkel@molkmedia.dk>
2026-04-06 17:21:30 +0200

    hotjar - marketing

--- marketing/app/layout.tsx
+++ marketing/app/layout.tsx
@@ -77,6 +77,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   const gtmId = 'GTM-5FLVBF65'
+  const hotjarId = process.env.NEXT_PUBLIC_HOTJAR_ID
+  const hotjarSv = process.env.NEXT_PUBLIC_HOTJAR_SV || '6'
+  const shouldEnableHotjar = Boolean(hotjarId)
 
   return (
     <html lang="en">
@@ -100,6 +103,24 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             style={{ display: 'none', visibility: 'hidden' }}
           />
         </noscript>
+        {shouldEnableHotjar && (
+          <Script
+            id="hotjar-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:${hotjarId},hjsv:${hotjarSv}};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+              `,
+            }}
+          />
+        )}
         {children}
       </body>
     </html>
